5be999b956fb8ac7f198f90abcf401a553d9cf56
Jonathan Bouwer <Jonathan.Bouwer1@gmail.com>
2017-02-15 12:38:53 +0200

    Initial commit

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off 
+if exist bin\ goto compile
+mkdir bin 
+:compile
+cls
+set WarningOptions=-W4 -WX -wd4100 -wd4189
+set CompilerOptions=-EHsc -nologo -Zi %WarningOptions%
+set LinkerOptions=
+pushd bin  
+cl %CompilerOptions% ..\src\main.cpp -link %LinkerOptions%
+popd 
